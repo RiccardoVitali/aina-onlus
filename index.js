e5ed860ee462d74c57cb76e4923b4c06eba0ef71
Riccardo Vitali <riccardovitali23@gmail.com>
2020-05-19 11:51:24 +0200

    provando a connettere postgres

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 'use strict';
 
+const { Client } = require('pg');
+
+const client = new Client({
+  //connectionString: process.env.DATABASE_URL,
+  connectionString:'postgressql://fabio:sailor@localhost:5432/demo',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+/*
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});*/
+
+
 //let pathname = window.location.pathname;
 //console.log(pathname);
 var fs = require('fs'),
