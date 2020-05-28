e15585f3c55e92b9d7504e04f98837a07944b2e7
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-05-28 17:28:35 +0200

    Update EmailService.js

--- service/EmailService.js
+++ service/EmailService.js
@@ -1,59 +1,19 @@
 'use strict';
 
 
- const sqlDbFactory = require("knex");
- let sqlDb = sqlDbFactory({
-   client: "pg",
-   debug: true,
-   //connection: process.env.DATABASE_URL,
-   connection:'postgressql://federicopozzi:semplice@localhost:5433/template1',
-   ssl: true
- });
-
-
-exports.emailPOST = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "email" : "email"
-}, {
-  "email" : "email"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
-}
-
-/*
-app.post('/', async (req, res) => {
-  let message = [];
-  let email = req.body;
-  pool.query(
-    `select * from email
-    where email = $1`,[email],(err, results) => {
-      if (err){
-        throw err;
-      }
-      if(results.rows.length > 0){
-        message.push("This email is already registered for the newsletter");
-        res.render("newsletter",{message});
-      }
-      else{
-        pool.query(
-          `insert into email (email)
-          values ($1)`, [email], (err, results) => {
-            if (err){
-              throw err
-            }
-            message.push("You are registered for the newsletter!");
-            res.render("newsletter",{message});
-          }
-        );
-      }
-    }
-  );
+const sqlDbFactory = require("knex");
+let sqlDb = sqlDbFactory({
+  client: "pg",
+  debug: true,
+  //connection: process.env.DATABASE_URL,
+  connection:process.env.DATABASE_URL || 'postgressql://federicopozzi:semplice@localhost:5433/template1',
+  ssl: true
 });
-*/
+
+
+ exports.emailGET = function() {
+   return sqlDb("email")
+   .then(data => {
+     return data;
+   });
+ };
