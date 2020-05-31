9c3193b2436f31daac99888d5c38e5346a5e64b6
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-05-31 13:23:11 +0200

    Create contact_form.js

---
+++ assets/js/contact_form.js
@@ -0,0 +1,29 @@
+function contact(){
+  //window.location = popo;
+  var email = document.forms["contact-form"]["email"].value;
+  var name = document.forms["contact-form"]["name"].value;
+  var subject = document.forms["contact-form"]["subject"].value;
+  var message = document.forms["contact-form"]["message"].value;
+
+  const m = [name, email, subject, message];
+
+  fetch('/contact' , {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(m)
+  })
+  .then(res => {
+
+    if(res.status==402){
+    alert("All fields must be completed");
+    }
+
+    if(res.status==301){
+    alert("Hi "+ name +", we've received your message about "+subject+". We'll answer you at "+email);
+    }
+
+  });
+
+}
