69d2c798b329f42890ba3692f4701e33cf2c4492
andreasilvano <50112177+andreasilvano@users.noreply.github.com>
2020-06-03 23:18:51 +0200

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,7 +3,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
     <div class="row mr-5 ml-5">
 
       <div class="col-lg-4 col-md-6 footer-contact">
-        <h5>TERMS & CONDITION</h5>
+        <h5>Terms & Conditions</h5>
         <p>
           We employ the use of cookies. <br>
           By accessing aina-onlus.it, you <br>
@@ -13,7 +13,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
       </div>
       <div class="col-lg-4 col-md-6 footer-contact">
         <a href="/supportus.html">
-        <h5 style="color:white;">CONTACT US</h5>
+        <h5 style="color:white;">Contact us</h5>
       </a>
         <p>
 
@@ -23,15 +23,22 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
       </div>
 
       <div class="col-lg-4 col-md-6 footer-newsletter">
-        <h5>NEWSLETTER</h5>
-<!--
-        <form name="newsletter" action="/newsletter" method="post" onsubmit="return check()">
-          <input type="email" name="email"><input type="submit" value="Subscribe">
-        </form>
-      </div>-->
-      <form id="email_form" name="email_form" onsubmit="check()">
+      <div class="container-fluid">
+  <div class="row justify-content-center">
+
+
+              <h5 class="sub-n"> Subscribe to the newsletter </h5>
+              <div class="row d-flex my-2 pr-2 pr-md-5 div1">
+                  <div class="col-9"> <input type="email" class="form-control py-3" id="inp1" placeholder="Enter email address"> </div>
+                  <div class="col-3 px-0"> <button class="btn-foot text-white px-4 py-2" > <span style="color:black">OK</span> </button> </div>
+
+
+      </div>
+  </div>
+</div>
+      <!-- <form id="email_form" name="email_form" onsubmit="check()">
         <input type="email" name="email"><input type="submit" value="Subscribe">
-      </form>
+      </form> -->
     </div>
 
     </div>
