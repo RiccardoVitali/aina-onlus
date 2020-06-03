79c42db5f1570a80759fffa3b245720525e57a90
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-06-03 15:47:05 +0200

    Update UserService.js

--- service/UserService.js
+++ service/UserService.js
@@ -9,23 +9,25 @@ let sqlDb = sqlDbFactory({
   connection:'postgressql://federicopozzi:semplice@localhost:5433/template1',
   ssl: true
 });
-/**
- * Login
- * Login with a form
- *
- * username String
- * password String
- * no response value expected for this operation
- **/
-exports.usersLoginPOST = function(username,password) {
-  return new Promise(function(resolve, reject) {
-    resolve();
-  });
-}
 
-exports.usersMeGET = function() {
-  return sqlDb("users")
-  .then(data => {
-    return data;
-  });
-}
+exports.userGET = function() {
+   console.log("userGET")
+   return sqlDb("users")
+   .then(data => {
+     return data;
+   });
+ };
+ exports.user_by_nameGET = function(username) {
+   console.log("user_by_nameGET")
+
+   //console.log(username);
+   return sqlDb("users")
+   .then(data => {
+     for(var i=0;i<data.length;i++){
+       if(data[i].username==username){
+         return data[i];
+       }
+     }
+     return false;
+   });
+ };
