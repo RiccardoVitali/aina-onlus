a4594445dfc90e2ebdf63db8a7be2ff0d433eb15
federicopozzi <65979192+federicopozzi@users.noreply.github.com>
2020-06-06 11:24:16 +0200

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,7 +3,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
     <div class="row mr-5 ml-5">
 
       <div class="col-lg-4 col-md-6 footer-contact">
-        <h5>Terms & Conditions</h5>
+        <h4 style="color:white">Terms & Conditions</h4>
         <p>
           We employ the use of cookies. <br>
           By accessing aina-onlus.it, you <br>
@@ -13,7 +13,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
       </div>
       <div class="col-lg-4 col-md-6 footer-contact">
         <a href="/supportus.html">
-        <h5 style="color:white;">Contact us</h5>
+        <h4 style="color:white;">Contact us</h4>
       </a>
         <p>
 
@@ -26,7 +26,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
       <div class="container-fluid">
   <div class="row justify-content-center">
 
-     <h5 class="sub-n"> Subscribe to the newsletter </h5>
+     <h4 class="sub-n" style="color:white"> Subscribe to the newsletter </h4>
           <form id="email_form" name="email_form" onsubmit="check()">
               <div class="row d-flex my-2 pr-2 pr-md-5 div1">
                   <div class="col-9"> <input type="email" name="email" class="form-control py-3" id="inp1" placeholder="Enter email address"> </div>
