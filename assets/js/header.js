d91e4a2e3f827cad4cc559aa47e32fd43363187d
federicopozzi <65979192+federicopozzi@users.noreply.github.com>
2020-06-06 17:35:17 +0200

    Add files via upload

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,67 +1,91 @@
-// fetch("v1/users/me").then(function(response){
-//
-//   response.json().then(res=>{
-//
-//     if(res.error!=undefined){
-//       document.getElementById("header").innerHTML = `<div class="container-fluid mr-5 ml-5 d-flex align-items-center">
-//
-//       <div class="logo mr-auto">
-//
-//         <a href="/"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
-//       </div>
-//
-//       <nav class="nav-menu d-none d-lg-block">
-//         <ul>
-//           <li><a href="/allservices.html">Services</a></li>
-//           <li><a href="/allevents.html">Events</a></li>
-//           <li><a href="/allpeople.html">People</a></li>
-//           <li><a href="/association.html">Association</a></li>
-//           <li><a href="/supportus.html">Support Us</a></li>
-//           <li><a href="/contactus.html">Contact Us</a></li>
-//
-//           <li><a href="/login.html">  <span id="login">Log in</span> </a></li>
-//
-//
-//         </ul>
-//       </nav>
-//
-//
-//
-//     </div>`
-//
-//       return;
-//     }
-//     else{
-      document.getElementById("header").innerHTML = `<div class="container-fluid mr-5 ml-5 d-flex align-items-center">
+fetch("v1/users/me").then(function(response){
 
-      <div class="logo mr-auto">
+  response.json().then(res=>{
 
-        <a href="/"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
-      </div>
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li><a href="/allservices.html">Services</a></li>
-          <li><a href="/allevents.html">Events</a></li>
-          <li><a href="/allpeople.html">People</a></li>
-          <li><a href="/association.html">Association</a></li>
-          <li><a href="/supportus.html">Support Us</a></li>
-          <li><a href="/contactus.html">Contact Us</a></li>
-
-          <li><a href="/dash.html">  <span id="login">MY AINA</span> </a></li>
-          <li><form style="padding-top:5px;" method="post" action="v1/users/logout">
-                      <input id="login" type="submit" value="Logout" />
-                    </form></li>
+    if(res.error!=undefined){
+      document.getElementById("header").innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light ml-5 mr-5">
 
 
-        </ul>
-      </nav>
+        <a class="navbar-brand" href="#">
+          <img src="assets/img/logo.png" width="150" height="150" class="d-inline-block align-top" alt="">
+
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Services <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">People</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Association</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Support Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Contact Us</a>
+            </li>
+
+          </ul>
+          <form class="form-inline">
+        <button class="btn btn-danger" type="button">Log In</button>
+        </div>
+
+      </nav>`
+
+      return;
+    }
+    else{
+      document.getElementById("header1").innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light ml-5 mr-5">
 
 
+        <a class="navbar-brand" href="#">
+          <img src="assets/img/logo.png" width="150" height="150" class="d-inline-block align-top" alt="">
 
-    </div>`
-//
-//       return ;
-//     }
-//   })
-// });
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item ">
+              <a class="nav-link" href="#">Services <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">People</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Association</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Support Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Contact Us</a>
+            </li>
+
+          </ul>
+          <form class="form-inline">
+        <button class="btn btn-danger" type="button">Log Out</button>
+        <button class="btn btn-danger" type="button">My aina</button>
+        </div>
+
+      </nav>`
+
+      return ;
+    }
+  })
+});
