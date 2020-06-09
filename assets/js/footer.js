4bf01fc2ca6fc71345d05866746a2c2ed107d05a
federicopozzi <65979192+federicopozzi@users.noreply.github.com>
2020-06-09 23:37:45 +0200

    Add files via upload

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,7 +3,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
     <div class="row mr-5 ml-5">
 
       <div class="col-lg-4 col-md-6 footer-contact">
-        <h4 style="color:white">Terms & Conditions</h4>
+        <h3 style="color:white">Terms & Conditions</h3>
         <p>
           We employ the use of cookies. <br>
           By accessing aina-onlus.it, you <br>
@@ -13,12 +13,12 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
       </div>
       <div class="col-lg-4 col-md-6 footer-contact">
         <a href="/supportus.html">
-        <h4 style="color:white;">Contact us</h4>
+        <h3 style="color:white;">Contact us</h3>
       </a>
         <p>
 
-          <strong>Phone:</strong> +1 5589 55488 55<br>
-          <strong>Email:</strong> hello@aina-onlus.it<br>
+          <span uk-icon="receiver"></span>&nbsp +1 5589 55488 55<br>
+          <span uk-icon="mail"></span>&nbsp hello@aina-onlus.it<br>
         </p>
       </div>
 
@@ -26,7 +26,7 @@ document.getElementById("footer").innerHTML = `<div class="footer-top ">
       <div class="container-fluid">
   <div class="row justify-content-center">
 
-     <h4 class="sub-n" style="color:white"> Subscribe to the newsletter </h4>
+     <h3 class="sub-n" style="color:white"> Subscribe to the newsletter </h3>
           <form id="email_form" name="email_form" onsubmit="check()">
               <div class="row d-flex my-2 pr-2 pr-md-5 div1">
                   <div class="col-9"> <input type="email" name="email" class="form-control py-3" id="inp1" placeholder="Enter email address"> </div>
