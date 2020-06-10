865215b5ce8e6d9a53bea0e87199920a2af24d2e
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-06-10 12:07:55 +0200

    Update UserServiceSwagger.js

--- service/UserServiceSwagger.js
+++ service/UserServiceSwagger.js
@@ -2,22 +2,17 @@
 var User = require('../service/UserService');
 
 exports.usersLoginPOST = function(username,password) {
-  console.log("usersLoginPOST");
   return new Promise(function(resolve, reject) {
     User.user_by_nameGET(username).then(rows => {
 
       if(rows==false){
-        console.log("non esiste un account con questo username");
         resolve(rows);
       }
       else{
         if(rows.password==password){
-          console.log("password corretta");
-          console.log(rows);
           resolve(rows);
         }
         else{
-          console.log("password errata");
           resolve(true);
         }
       }
@@ -25,12 +20,10 @@ exports.usersLoginPOST = function(username,password) {
   });
 };
 exports.usersMeGET = function(x) {
-  console.log("usersMeGET");
   return new Promise(function(resolve, reject) {
 
     User.user_by_nameGET(x).then(rows => {
-      console.log("r");
-      console.log(rows);
+
       resolve(rows);
     })
   });
