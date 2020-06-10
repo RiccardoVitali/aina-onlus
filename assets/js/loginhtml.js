8ca986eb0677a937bfa38a826d90df4ba9ea7a02
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-06-10 11:30:01 +0200

    Update loginhtml.js

--- assets/js/loginhtml.js
+++ assets/js/loginhtml.js
@@ -1,8 +1,5 @@
  fetch("v1/users/me").then(function(response) {
 
-    console.log("response html");
-    console.log(response)
-
     response.json().then(res => {
       if (response.status == 303) {
         return {
