baf41186e1533b6057850bc89afd2d4b6160c425
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-06-16 11:20:57 +0200

    Update ServiceService.js

--- service/ServiceService.js
+++ service/ServiceService.js
@@ -1,12 +1,10 @@
 'use strict';
 
-//let sqlDb = require("./DataLayer.js")
 const sqlDbFactory = require("knex");
 let sqlDb = sqlDbFactory({
   client: "pg",
   debug: true,
-  //connection: process.env.DATABASE_URL,
-  connection:process.env.DATABASE_URL || 'postgressql://federicopozzi:semplice@localhost:5433/template1',
+  connection: process.env.DATABASE_URL,
   ssl: true
 });
 
