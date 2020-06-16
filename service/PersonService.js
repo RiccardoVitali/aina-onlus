3d888bebe8f4900b73cdb9f6e02e34d10e77e82d
RiccardoVitali <59535524+RiccardoVitali@users.noreply.github.com>
2020-06-16 11:19:42 +0200

    Update PersonService.js

--- service/PersonService.js
+++ service/PersonService.js
@@ -1,13 +1,11 @@
 'use strict';
 
 
-//CHIEDI COME SI FA A FARLO PIU CARINO TIPO SERVICESERVICE.JS
 const sqlDbFactory = require("knex");
 let sqlDb = sqlDbFactory({
   client: "pg",
   debug: true,
-  //connection: process.env.DATABASE_URL,
-  connection:process.env.DATABASE_URL || 'postgressql://federicopozzi:semplice@localhost:5433/template1',
+  connection: process.env.DATABASE_URL,
   ssl: true
 });
 
